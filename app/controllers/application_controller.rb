b547fc5620a5ec1953a40da9fd5169d83c236d65
Brian Scott <brianscott0017@yahoo.com>
2015-11-08 17:11:02 -0500

    nov-7-510

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,10 +5,10 @@ class ApplicationController < ActionController::API
   
     def get_resource(resource_type, id)
 		fhirbase_query_result =  ActiveRecord::Base.connection.execute("SELECT fhir.read('#{resource_type}', '#{id}');")
-		puts fhirbase_query_result.fname(1)
-		puts fhirbase_query_result.fname(2)
-		content_col_index = fhirbase_query_result.fnumber('content')
-		json_content = fhirbase_query_result.getvalue(1, 2)
+		#puts fhirbase_query_result.fname(1)
+		#puts fhirbase_query_result.fname(2)
+		#content_col_index = fhirbase_query_result.fnumber('content')
+		json_content = fhirbase_query_result.getvalue(0, 1)
 		puts 'line 10'
 		puts json_content.class
 		result = json_content
