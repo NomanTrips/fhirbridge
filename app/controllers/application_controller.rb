1c4a6cb72a530ad2d29ac5227a4d05bb9b5684d2
Brian Scott <brianscott0017@yahoo.com>
2015-11-12 10:41:20 -0500

    nov-12-1041

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,32 +4,11 @@ class ApplicationController < ActionController::API
   #protect_from_forgery with: :exception
   
     def get_resource(resource_type, id)
-		res =  ActiveRecord::Base.connection.execute("SELECT fhir.read('#{resource_type}', '#{id}');")
-		#puts fhirbase_query_result.fname(1)
-		#puts fhirbase_query_result.fname(2)
-		#content_col_index = fhirbase_query_result.fnumber('content')
-		puts 'after query exec...'
-		puts 'res class: ' + res.class.to_s
-		
-		res.each{|tuple| puts tuple.length}
-		
-		res_hash = res[0]
-		record_hash = res_hash.first
-		puts 'record_hash class:' + record_hash.class.to_s
-		result = record_hash.second
-		puts 'result class: ' + result.class.to_s
-		
-		#puts 'done printing the field names.'
-		#puts res[0].size
-		#json_content = fhirbase_query_result.getvalue(0, 1)
-		#puts 'line 10'
-		#puts json_content.class
-		#result = json_content
-		#record = result.to_a()
-		#result = record.first
-		#record_a = result.first
-		#json_content_str = record_a.second
-		#result = json_content_str
+		res =  ActiveRecord::Base.connection.execute("SELECT fhir.read('#{resource_type}', '#{id}');") # Running fhirbase stored procedure
+		puts res.cmd_status()
+		res_hash = res[0] #First row of query result
+		record_hash = res_hash.first #Some kind of wrapper array?
+		result = record_hash.second #string of the json content
 	return result
   end
   
