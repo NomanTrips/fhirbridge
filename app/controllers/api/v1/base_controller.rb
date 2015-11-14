8d87bab06bf90a10496c98695102452de9c583e6
Brian Scott <brianscott0017@yahoo.com>
2015-11-14 17:05:59 -0500

    nov-14-505

--- app/controllers/api/v1/base_controller.rb
+++ app/controllers/api/v1/base_controller.rb
@@ -10,6 +10,20 @@ class Api::V1::BaseController < ApplicationController
 	#render json: JSON.pretty_generate( get_resource(params[:resource_type], params[:id]) )
   end
 
+  # POST /api/{plural_resource_name}
+  def create
+	puts 'entering create....'
+	puts request.body.read
+	render json: create_resource(request.body.read)
+
+	#if get_resource.save
+	 # render :show, status: :created
+	#else
+	 # render json: get_resource.errors, status: :unprocessable_entity
+	#end
+  
+  end
+
   def destroy_session
     request.session_options[:skip] = true
   end
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,8 +5,8 @@ class ApplicationController < ActionController::API
   
     def get_resource(resource_type, id)
 		res =  ActiveRecord::Base.connection.execute("SELECT fhir.read('#{resource_type}', '#{id}');") # Running fhirbase stored procedure
-		puts 'res status: ' + res.cmd_status()
-		puts 'tuple amt: ' + res.ntuples().to_s
+		#puts 'res status: ' + res.cmd_status()
+		#puts 'tuple amt: ' + res.ntuples().to_s
 		
 		if res.ntuples() > 0 then
 			res_hash = res[0] #First row of query result
@@ -17,5 +17,18 @@ class ApplicationController < ActionController::API
 	return result
   end
   
+	def create_resource(payload)
+	# SELECT fhir.create('{"resourceType":"Patient", "name": [{"given": ["John"]}]}')
+		res =  ActiveRecord::Base.connection.execute("SELECT fhir.create('#{payload}');") # Running fhirbase stored procedure
+
+		if res.ntuples() > 0 then
+			res_hash = res[0] #First row of query result
+			record_hash = res_hash.first #Some kind of wrapper array?
+			result = record_hash.second #string of the json content
+		end
+	
+		return result
+	
+	end
   
 end
