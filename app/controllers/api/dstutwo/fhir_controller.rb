e0db9fd8da722b62a9634afd49ceacfbbfcad5c1
Brian Scott <brianscott0017@yahoo.com>
2016-02-21 21:40:13 -0500

    added cross origin crendtials header

--- app/controllers/api/dstutwo/fhir_controller.rb
+++ app/controllers/api/dstutwo/fhir_controller.rb
@@ -14,17 +14,19 @@ class Api::Dstutwo::FhirController < ApplicationController
 
   def cors_set_access_control_headers
     headers['Access-Control-Allow-Origin'] = '*'
-    headers['Access-Control-Allow-Methods'] = 'POST, GET, PUT, DELETE, OPTIONS'
-    headers['Access-Control-Allow-Headers'] = 'Origin, Content-Type, Accept, Authorization, Token'
+    headers['Access-Control-Allow-Methods'] = 'GET, HEAD, POST, PUT, DELETE, TRACE, OPTIONS'
+    headers['Access-Control-Allow-Headers'] = 'origin, authorization, accept, content-type, x-requested-with'
     headers['Access-Control-Max-Age'] = "1728000"
+    headers['Access-Control-Allow-Credentials'] = "true"
   end
 
   def cors_preflight_check
     if request.method == 'OPTIONS'
       headers['Access-Control-Allow-Origin'] = '*'
-      headers['Access-Control-Allow-Methods'] = 'POST, GET, PUT, DELETE, OPTIONS'
-      headers['Access-Control-Allow-Headers'] = 'X-Requested-With, X-Prototype-Version, Token'
+      headers['Access-Control-Allow-Methods'] = 'GET, HEAD, POST, PUT, DELETE, TRACE, OPTIONS'
+      headers['Access-Control-Allow-Headers'] = 'origin, authorization, accept, content-type, x-requested-with'
       headers['Access-Control-Max-Age'] = '1728000'
+      headers['Access-Control-Allow-Credentials'] = "true"
 
       render :text => '', :content_type => 'text/plain'
     end
