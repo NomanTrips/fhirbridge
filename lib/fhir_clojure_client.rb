6d9132820ac8888dd88f013ba3517ed5100e21a1
Brian Scott <brianscott0017@yahoo.com>
2016-02-23 10:53:11 -0500

    cross compare body

--- lib/fhir_clojure_client.rb
+++ lib/fhir_clojure_client.rb
@@ -52,7 +52,6 @@ class FhirClojureClient
 				
 		payload_parsed = @@fhir_core.parse(idx, resource_as_xml_str)			
 		resource_as_json_str = @@fhir_core.generate(idx, @@clojure_core.keyword("json"), payload_parsed)	
-		puts resource_as_json_str.class.name
 		return resource_as_json_str.to_s
 		
 	end
