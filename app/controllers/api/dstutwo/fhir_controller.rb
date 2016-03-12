404460fdbc204a3dff3379d49bd0d9e6d07ae775
Brian Scott <brianscott0017@yahoo.com>
2016-03-12 18:36:35 -0500

    using older vewrsion of puma

--- app/controllers/api/dstutwo/fhir_controller.rb
+++ app/controllers/api/dstutwo/fhir_controller.rb
@@ -16,7 +16,7 @@ class Api::Dstutwo::FhirController < ApplicationController
   def cors_set_access_control_headers
     headers['Access-Control-Allow-Origin'] = '*'
     headers['Access-Control-Allow-Methods'] = 'GET, HEAD, POST, PUT, DELETE, TRACE, OPTIONS'
-    headers['Access-Control-Allow-Headers'] = 'origin, authorization, accept, content-type, x-requested-with'
+    headers['Access-Control-Allow-Headers'] = 'origin, authorization, accept, content-type, x-requested-with, Authorization'
     headers['Access-Control-Max-Age'] = "1728000"
     headers['Access-Control-Allow-Credentials'] = "true"
   end
