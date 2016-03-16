94ea7af737487478bc6768e55dc5004688aaf203
Brian Scott <brianscott0017@yahoo.com>
2016-03-16 10:18:21 -0400

    switched size to n tuples for pg result

--- lib/postgres_calls.rb
+++ lib/postgres_calls.rb
@@ -37,7 +37,7 @@ module PostgresCalls
 	end
 	puts res.size().to_s
 	resource_as_json_str = ''
-	if res.size() > 0 then
+	if res.num_tuples() > 0 then
 		res_hash = res[0] #First row of query result
 		record_hash = res_hash.first #Some kind of wrapper array?
 		resource_as_json_str = record_hash.second #string of the json content
